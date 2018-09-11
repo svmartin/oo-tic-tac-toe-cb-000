06c49e12e79e06dd1ec1908da7fac04954fa7e42
Sean Martin <sean.martin@gmail.com>
2018-09-11 15:08:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,12 @@ class TicTacToe
   def initialize
     @board = Array.new(9, " ")
   end
+
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n"
+  puts "-----------\n"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} \n"
+  puts "-----------\n"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+end
 end
